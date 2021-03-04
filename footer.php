524a201ebc43aa4c720345ccb5a3f224d1be7fad
Tonmoy <tonmoy142@gmail.com>
2021-03-04 16:44:54 +0600

    additional file

--- footer.php
+++ footer.php
@@ -103,7 +103,6 @@
     <script src="js/jquery.counterup.min.js"></script>
 	<script src="js/fancybox.min.js"></script>
 	<script src="js/main.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
 
 </body>
 
